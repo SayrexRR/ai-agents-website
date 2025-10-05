22394fe50c666168384f61806cc0d0d82cf4afa3
Oleksandr Zinchenko <rr.sayrex@gmail.com>
2025-10-05 22:42:10 +0300

    Added Vercle Analistic

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col font-sans">
       <Navbar />
-        <main className="flex-grow pt-20">
-          {children}
-          <SpeedInsights />
-        </main>
+      <main className="flex-grow pt-20">
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </main>
       <Footer />
     </div>
   );
